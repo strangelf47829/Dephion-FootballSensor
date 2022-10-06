cd954914e5322cc463842c87d1a87b2596853768
strangelf47829 <strangelf47829@gmail.com>
2022-10-06 12:22:02 +0200

    P1R2 and P2R1 added

---
+++ Software/TechSense/TechSense/TechSense/SerialManager.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO.Ports;
+
+
+namespace TechSense
+{
+    public class SerialManager
+    {
+        public static SerialPort ret;
+        public static SerialPort GetConnection(int addr) 
+        {
+            ret = new SerialPort("COM7", 9600);
+            ret.Open();
+            ret.WriteLine("connect:" + addr);
+
+            ret.DataReceived += Read;
+
+            ret.ReadTimeout = 500;
+            ret.WriteTimeout = 500;
+
+            return ret;
+        }
+
+
+        public static void Read(object sender, SerialDataReceivedEventArgs args) 
+        {
+            try
+            {
+                string s = ret.ReadLine();
+                MessageBox.Show(s);
+
+            }
+            catch(TimeoutException e) { }
+        }
+
+    }
+}
